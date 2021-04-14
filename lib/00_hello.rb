c138795b4204c8f29f8fcac4697e3866e062fb0d
Lionel Humbert <helloliohumb@gmail.com>
2021-04-14 18:29:43 +0200

    Hello

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,1 +1,20 @@
-puts "Hello"
+def say_hello
+  puts "Hello"
+end
+
+def first_name
+  puts "Comment tu t'appelles?"
+  print ">"
+  first_name = gets.chomp
+  puts "Hello, #{first_name}"
+end
+
+def ask_first_name
+  puts "Comment tu t'appelles?"
+  print ">"
+  ask_first_name = gets.chomp
+  return ask_first_name
+end
+
+say_hello
+first_name
